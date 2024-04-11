8ad487b589cde6920c7487a14c24f5e2af8c2609
Dario <orquera10@gmail.com>
2024-04-11 08:18:08 -0300

    mod

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path={"/"} element={<Calendar/>} />
-          <Route path={"/login"} element={<Login/>} />
+          <Route path={"/calendar"} element={<Calendar/>} />
+          <Route path={"/"} element={<Login/>} />
         </Routes>
       </BrowserRouter>
     </div>
